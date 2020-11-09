d91074a0951a0d10c1dac7f5b8f1d3f10f99d261
Danial Kafeel <danialkafeel@gmail.com>
2020-11-09 15:55:42 +0530

    Download File functionality added

--- flaskk.py
+++ flaskk.py
@@ -1,5 +1,7 @@
 # from flask import Flask, jsonify, render_template, request
 from flask import *
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,8 +10,17 @@ def home():
 
 @app.route('/receive_data',methods =['POST'])
 def fun():
-	print("sdfadsf = ",request.form['input_data'])
-	return redirect('/')
+	# print("sdfadsf = ",request.form['input_data'])
+	file = open("temp.txt","w")
+	file.write(request.form['input_data'])
+	return redirect('/download')
+
+@app.route('/download')
+def downloadFile ():
+    path = os.getcwd()+"/temp.txt"
+    print("Sending file...")
+    return send_file(path, as_attachment=True)
+
 
 if __name__ == '__main__':
 	app.run(port = 8888, debug = True)
