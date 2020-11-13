4222ebb2bd5d8a41e1af6ec7fc149cefdf6c8b99
Satyam Singh <satyam010singh@gmail.com>
2020-11-13 14:01:49 +0530

    Advanced search for home page

--- springer_crawler.py
+++ springer_crawler.py
@@ -1,66 +1,76 @@
 from bs4 import BeautifulSoup as soup
 import requests
 import json
-
-NA = "Not Available"
-http = 'https://www.springer.com'
-
-#---------------------------------------get the number of pages in the search result to loop over----------------------------------------------------
-stringToSearch = "Sulphur"
-html = requests.get('https://www.springer.com/in/search?dnc=true&facet-type=type__book&query=' + stringToSearch + '&submit=Submit').text
-page_soup = soup(html,'html.parser')
-noOfSearchPages = page_soup.find('span', {'class': 'number-of-pages'}).string.strip()
-noOfSearchPages = int(noOfSearchPages)+1
+import math
 
 #list of Parsed BibTex dictionary, each item can become vaule part of JSON object
 bibs = []
 
-#--------------------------------------for each page and then for each result in the page do bibTex processing----------------------------------------
-for page in range (1, noOfSearchPages):
-  html = requests.get('https://www.springer.com/in/search?dnc=true&facet-type=type__book&page=' + str(page) + '&query=' + stringToSearch +'&submit=Submit').text
-  page_soup = soup(html,'html.parser')
+#prefix link for search
+springerLink = 'https://www.springer.com/generic/search/results?SGWID=5-40109-24-653415-0&media=book&sortOrder=relevance&searchType=ADVANCED_CDA&searchScope=editions'
 
-  result = page_soup.findAll('div', {'class': 'result-item'})   #html-container of items in search-result
-  #------------------------------------------------for each search-result item------------------------------------------------------
-  for item in result:
-    link = http + item.find('h4').find('a')['href']   #link to the item's landing page
-    itemLandingPage = requests.get(link).text
-    itemSoup = soup(itemLandingPage, "html.parser")   #get item landing page HTML
-    bibliographySection = itemSoup.find('div', {'class':'product-bibliographic'})   #this section has DOI
-    divDOI = bibliographySection.findAll('dl')[2]
-    DOI = divDOI.findAll('dd')[1].string.strip()      #get DOI
+#parameters to filter
+queryText = 'quantum'
+title = ''
+author = 'henry'
+isbn = ''
 
-    target = 'https://citation-needed.springer.com/v2/references/' + DOI + '_1?format=bibtex&flavour=citation'  #link for BibTex
-    bibTex = requests.get(target)   #get BibTex
+#detailed link for search
+searchLink = springerLink + '&queryText=' + queryText + '&title=' + title + '&authorEditor=' + author + '&isbnIssn=' + isbn
 
-    #--------------------------------------if request is successful then parse the bibtex to a dictionary----------------------------
-    if(bibTex.status_code == 200):  
-      bibtex = bibTex.text.replace("}","").strip("\t \n ") + '\n'
-      ind = bibtex.index("{")
-      bib_dict = {}                           #dictionry equivalent of current BibTex object. has fields(type, id, other_attributes...)
-      bib_dict["type"] = bibtex[:ind]         #type, key value
-      bibtex = bibtex[ind+1:]
-      ind = bibtex.index("\n")
-      bib_dict["id"] = bibtex[:ind-1]         #id, key value
-      bibtex = bibtex[ind+1:]
+html = requests.get(searchLink).text
+page_soup = soup(html,'html.parser')
 
-      #-------------------get all other attributes, key value from bibtex (text form)---------------------
-      while(bibtex != "" and bibtex != "\n"):
-        ind = bibtex.index("=")
-        key = bibtex[:ind].strip() #key
-        bibtex = bibtex[ind+2:]
-        ind = bibtex.index("\"")
-        value = bibtex[:ind].replace('\n','').strip() #value
-        bib_dict[key] = value
-        bibtex = bibtex[ind+2:]
-      bibs.append(bib_dict)         #add current bibtex dictionary to global list
-#print(bibs)
+#get the number of pages in the search result to loop over
+noOfItemsInResults = int(page_soup.find('span', {'class' : 'resultInfo'}).text.split(' ')[2].replace(',', ''))
+print('no of items in search result :',noOfItemsInResults)
+noOfPages = math.ceil(noOfItemsInResults/10)
+print('no of pages in search result :',noOfPages)
 
-#-----------------------------------------------------file writing stuff----------------------------------------------------------------
-output_json = {}                    #json equivalent of the list of bibtex
-for bib in range(0, len(bibs)):
-  output_json[bib] = bibs[bib]
+#this is id number of the first item in any page.. this would decided which page number to fetch. 
+#there are 10 results per pages so this offset is incremented by 10 on each page
+#Example : if pageOffset = 1 then pageNo = 1, if pageOffset = 11 then pageNo = 2.. and so on
+pageOffset = 1
 
-filename = "output.json"            #write to json file
-with open(filename, 'w') as outfile:
-    json.dump(output_json, outfile)
+#for each page and then for each result in the page do bibTex processing
+for page in range (1, noOfPages+1):
+    html = requests.get(searchLink + '&resultStart=' + str(pageOffset)).text
+    pageOffset += 10
+    page_soup = soup(html,'html.parser')
+
+    result = page_soup.findAll('li', {'class': 'listItemBooks'})   #html-container of items in search-result
+    #------------------------------------------------for each search-result item------------------------------------------------------
+    for item in result:
+        link = item.find('a')['href']   #link to the item's landing page
+        itemLandingPage = requests.get(link).text
+        itemSoup = soup(itemLandingPage, "html.parser")   #get item landing page HTML
+        bibliographySection = itemSoup.find('div', {'class':'product-bibliographic'})   #this section has DOI
+        divDOI = bibliographySection.findAll('dl')[2]
+        DOI = divDOI.findAll('dd')[1].string.strip()      #get DOI
+
+        target = 'https://citation-needed.springer.com/v2/references/' + DOI + '_1?format=bibtex&flavour=citation'  #link for BibTex
+        bibTex = requests.get(target)   #get BibTex
+
+        #--------------------------------------if request is successful then parse the bibtex to a dictionary----------------------------
+        if(bibTex.status_code == 200):  
+            bibtex = bibTex.text.replace("}","").strip("\t \n ") + '\n'
+            ind = bibtex.index("{")
+            bib_dict = {}                           #dictionry equivalent of current BibTex object. has fields(type, id, other_attributes...)
+            bib_dict["type"] = bibtex[:ind]         #type, key value
+            bibtex = bibtex[ind+1:]
+            ind = bibtex.index("\n")
+            bib_dict["id"] = bibtex[:ind-1]         #id, key value
+            bibtex = bibtex[ind+1:]
+            
+            #-------------------get all other attributes, key value from bibtex (text form)---------------------
+            while(bibtex != "" and bibtex != "\n"):
+                ind = bibtex.index("=")
+                key = bibtex[:ind].strip() #key
+                bibtex = bibtex[ind+2:]
+                ind = bibtex.index("\"")
+                value = bibtex[:ind].replace('\n','').strip() #value
+                bib_dict[key] = value
+                bibtex = bibtex[ind+2:]
+            bibs.append(bib_dict)         #add current bibtex dictionary to global list
+            print(bib_dict)
+print(bibs)
