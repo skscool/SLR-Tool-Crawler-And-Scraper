875583d2a49e44fd10782c5da2b85b5c3b9d0cae
Danial Kafeel <danialkafeel@gmail.com>
2020-11-20 21:23:34 +0530

    Uniform front end

--- static/landing.js
+++ static/landing.js
@@ -2,7 +2,7 @@ function downloading() {
     var messageBox = document.getElementById("message");
     messageBox.innerHTML = "Fetching results ...";
     messageBox.className = 'pass';
-    document.getElementById("topHeading").innerHTML = "Fetching results ...";
+    // document.getElementById("topHeading").innerHTML = "Fetching results ...";
     console.log("from downloading");
 }
 function updateYear(){
